28f1f17540d5c1b95d21501cc3e747b7f74ab694
lant <minjun2006@icloud.com>
2025-09-30 23:56:54 +0900

    Create 09C19.ino

---
+++ 09C19.ino
@@ -0,0 +1,115 @@
+// Arduino pin assignment
+#define PIN_LED  9
+#define PIN_TRIG 12
+#define PIN_ECHO 13
+
+// configurable parameters
+#define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
+#define INTERVAL 25       // sampling interval (unit: msec)
+#define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
+#define _DIST_MIN 100     // minimum distance to be measured (unit: mm)
+#define _DIST_MAX 300     // maximum distance to be measured (unit: mm)
+
+#define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
+#define SCALE (0.001 * 0.5 * SND_VEL)     // coefficent to convert duration to distance
+
+#define _EMA_ALPHA 0.5    // EMA weight of new sample (range: 0 to 1)
+                          // Setting EMA to 1 effectively disables EMA filter.
+
+#define N 3
+
+
+// global variables
+unsigned long last_sampling_time;   // unit: msec
+float dist_prev = _DIST_MAX;        // Distance last-measured
+float dist_ema;                     // EMA distance
+
+int len=1,idx=0,raw_arr[N],sorted_arr[N],tmp;
+
+void setup() {
+  // initialize GPIO pins
+  pinMode(PIN_LED,OUTPUT);
+  pinMode(PIN_TRIG,OUTPUT);
+  pinMode(PIN_ECHO,INPUT);
+  digitalWrite(PIN_TRIG, LOW);
+
+  // initialize serial port
+  Serial.begin(57600);
+}
+
+void loop() {
+  float dist_raw, dist_filtered;
+  
+  // wait until next sampling time. 
+  // millis() returns the number of milliseconds since the program started. 
+  // will overflow after 50 days.
+  if (millis() < last_sampling_time + INTERVAL)
+    return;
+
+  // get a distance reading from the USS
+  dist_raw = USS_measure(PIN_TRIG,PIN_ECHO);
+
+  dist_ema = _EMA_ALPHA*dist_raw+(1-_EMA_ALPHA)*dist_ema;
+  
+  raw_arr[idx]=dist_raw;
+  for(int i=0;i<len;i++){
+    sorted_arr[i]=raw_arr[i];
+  }
+//  qsort(sorted_arr,len,sizeof(int),cmp);
+  for(int i=0,tmp;i<len;i++){
+    for(int j=i+1;j<len;j++){
+      if(sorted_arr[i]>sorted_arr[j]){
+        tmp=sorted_arr[i];
+        sorted_arr[i]=sorted_arr[j];
+        sorted_arr[j]=tmp;
+      }
+    }
+  }
+  int dist_median;
+  if (len%2){
+    dist_median=sorted_arr[len/2];
+  }else{
+    dist_median=(sorted_arr[len/2]+sorted_arr[len/2+1])/2;
+  }
+  len++;
+  len=min(N,len);
+  idx++;
+  idx%=N;
+  
+  // output the distance to the serial port
+  Serial.print("Min:"); Serial.print(_DIST_MIN);
+  Serial.print(",raw:"); Serial.print(dist_raw);
+  Serial.print(",ema:"); Serial.print(dist_ema);
+  Serial.print(",median:"); Serial.print(dist_median);
+  Serial.print(",Max:"); Serial.print(_DIST_MAX);
+  Serial.println("");
+
+  // do something here
+  if ((dist_raw < _DIST_MIN) || (dist_raw > _DIST_MAX))
+    digitalWrite(PIN_LED, 1);       // LED OFF
+  else
+    digitalWrite(PIN_LED, 0);       // LED ON
+
+  // update last sampling time
+  last_sampling_time += INTERVAL;
+}
+
+// get a distance reading from USS. return value is in millimeter.
+float USS_measure(int TRIG, int ECHO)
+{
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(PULSE_DURATION);
+  digitalWrite(TRIG, LOW);
+  
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE; // unit: mm
+
+  // Pulse duration to distance conversion example (target distance = 17.3m)
+  // - pulseIn(ECHO, HIGH, timeout) returns microseconds (음파의 왕복 시간)
+  // - 편도 거리 = (pulseIn() / 1,000,000) * SND_VEL / 2 (미터 단위)
+  //   mm 단위로 하려면 * 1,000이 필요 ==>  SCALE = 0.001 * 0.5 * SND_VEL
+  //
+  // - 예, pusseIn()이 100,000 이면 (= 0.1초, 왕복 거리 34.6m)
+  //        = 100,000 micro*sec * 0.001 milli/micro * 0.5 * 346 meter/sec
+  //        = 100,000 * 0.001 * 0.5 * 346
+  //        = 17,300 mm  ==> 17.3m
+}
